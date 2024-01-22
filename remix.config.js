488ecdd13fe6479e9e49192ed9d9a33bde4fc8b8
Magnus Skulason <maggisk@gmail.com>
2024-01-22 22:56:57 +0000

    try setting serverBuildPath to use .js

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,12 @@ const { config } = require("@netlify/remix-adapter");
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ...(process.env.NODE_ENV === "production" ? config : undefined),
+  ...(process.env.NODE_ENV === "production" ? {
+    ...config,
+    // same as config config.serverBuildPath but with a .js extension in stead of .mjs
+    serverBuildPath: './.netlify/functions-internal/server.mjs',
+  } : undefined),
+
   serverModuleFormat: "cjs",
   // This works out of the box with the Netlify adapter, but you can
   // add your own custom config here if you want to.
