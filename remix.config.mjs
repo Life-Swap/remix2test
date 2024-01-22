38c2ccaa3d7296af101885247ad106a3c603ef47
Magnus Skulason <maggisk@gmail.com>
2024-01-22 23:22:14 +0000

    attempt to remove "type": "module" from package.json

--- remix.config.mjs
+++ remix.config.mjs
@@ -2,7 +2,12 @@ import { config } from "@netlify/remix-adapter";
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ...(process.env.NODE_ENV === "production" ? config : undefined),
+  ...(process.env.NODE_ENV === "production" ? {
+    ...config,
+    server: './server.mjs',
+  } : undefined),
+  serverBuildPath: "./build/index.mjs",
+  serverModuleFormat: "esm",
   // This works out of the box with the Netlify adapter, but you can
   // add your own custom config here if you want to.
   //
